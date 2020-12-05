a13cce6965803b2ba3a1bfd169af7682e8e60e85
Ben Rose <bcrose@bsu.edu>
2020-12-05 01:05:13 -0500

    Add project files.

---
+++ HelloWorldAPI/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloWorldAPI.Controllers
+{
+    public class DemoController : ApiController
+    {
+        public string Get(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
